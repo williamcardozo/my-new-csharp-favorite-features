cc5b7e309cc884d9289b6cdb543f49089cfd67c3
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2017-12-11 14:58:01 -0200

    sample of local function

--- MyNewCSharpFavoriteFeatures/LocalFunctions/LocalFunctionSample.cs
+++ MyNewCSharpFavoriteFeatures/LocalFunctions/LocalFunctionSample.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyNewCSharpFavoriteFeatures.LocalFunctions
 {
     public class LocalFunctionSample
     {
-      
+        public void MethodUsingLocalFunction()
+        {
+            Console.WriteLine(LocalFunction());
+
+            string LocalFunction() => "My Local Function";
+        }
     }
 }
